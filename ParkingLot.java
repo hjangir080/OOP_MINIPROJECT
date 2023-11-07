c582f680892dc1227b0f2b252b7bc9c2043ab277
hjangir080 <112872382+hjangir080@users.noreply.github.com>
2023-11-07 15:36:11 +0530

    Create ParkingLot.java

---
+++ ParkingLot.java
@@ -0,0 +1,27 @@
+package parking;
+
+import vehicles.Vehicle;
+
+public class ParkingLot {
+    private final ParkingFloor[] floors;
+
+    public ParkingLot(int floorsCount) {
+        floors = new ParkingFloor[floorsCount];
+        for (int i = 0; i < floorsCount; i++) {
+            floors[i] = new ParkingFloor(10); // Each floor has a capacity of 10 vehicles
+        }
+    }
+
+    public void parkVehicle(Vehicle vehicle) throws Exception {
+        floors[vehicle.getFloor()].parkVehicle(vehicle);
+    }
+
+    public Vehicle retrieveVehicle(int floor, String numPlate) {
+        if (floor >= 0 && floor < floors.length) {
+            return floors[floor].retrieveVehicle(numPlate);
+        } else {
+            System.out.println("Invalid floor number.");
+        }
+        return null;
+    }
+}
